ee2ff684dd0c15d9eac9f81fbf53bbabe940faf5
Леонид Харлов <harlov.leonid@ya.ru>
2023-12-09 07:46:21 +0300

    Fix: functions return GeoDataFrames now instead of GeoSeries

--- urban_scaling_toolkit/data_fetcher.py
+++ urban_scaling_toolkit/data_fetcher.py
@@ -26,8 +26,8 @@ def fetch_territory(territory_name):
     
     territory = ox.geocode_to_gdf(territory_name)
     territory = territory.set_crs(4326)
-    territory = territory["geometry"]
-    territory = territory.reset_index(drop=True)
+    territory = territory["geometry"].reset_index(drop=True)
+    territory = gpd.GeoDataFrame(territory)
     
     return territory
 
@@ -50,40 +50,10 @@ def fetch_buildings(territory, express_mode=True):
     except:
         buildings = buildings["geometry"].reset_index(drop=True)
     
+    buildings = gpd.GeoDataFrame(buildings)
+    
     return buildings
 
-
-def fetch_services(territory, service_tags=service_tags,subdivision=3,verbose=True):
-
-    if type(territory) in [gpd.GeoDataFrame,gpd.GeoSeries]:
-        territory = territory.unary_union
-        
-    res_list = []
-    
-    for category in tqdm(service_tags.columns, disable=not verbose):
-        tags = dict(service_tags[category].dropna())
-
-        try: 
-            services_temp = ox.features_from_polygon(territory,tags)    
-        except InsufficientResponseError: 
-            continue
-        except:
-            services_temp = fetch_long_query(territory,tags,subdivision)
-            
-        good_keys = list(set(tags.keys()).intersection(services_temp.columns))
-        services_temp_tags = services_temp[good_keys].reset_index(drop=True).apply(lambda x: list(x.dropna()), axis=1)
-        services_geometry = services_temp[['name','geometry'] if 'name' in services_temp else ['geometry']].reset_index(drop=True)
-        
-        services_temp = pd.concat([services_geometry,services_temp_tags],axis=1).reset_index(drop=True)
-        services_temp["category"] = category
-        services_temp = services_temp.rename(columns={0: "tags"})
-        res_list.append(services_temp)
-
-    res = pd.concat(res_list) if res_list else gpd.GeoDataFrame()
-    res["geometry"] = res.to_crs(3857)["geometry"].centroid.to_crs(4326)
-    res = res.reset_index(drop=True)
-
-    return res
 
 def fetch_long_query(territory, tags, subdivision=3,verbose=True):
     
@@ -108,24 +78,6 @@ def fetch_long_query(territory, tags, subdivision=3,verbose=True):
     return res
 
 
-def fetch_roads_long(territory, tags, subdivision=3,verbose=True):
-    cells = create_grid(territory,n_cells=subdivision) 
-    res_list = []
-    
-    for poly in tqdm(cells['geometry'],leave=False,disable=not verbose):
-        try: 
-            roads_in_cell = ox.features_from_polygon(poly, tags)
-        except InsufficientResponseError: 
-            continue
-        except:
-            roads_in_cell = fetch_roads_long(poly,tags,subdivision)
-        
-        if len(roads_in_cell) > 0: res_list.append(roads_in_cell)
-   
-    res = pd.concat(res_list) if res_list else gpd.GeoDataFrame()
-    
-    return res
-
 def fetch_roads(territory):
     tags = {
         "highway": ["construction","crossing","living_street","motorway","motorway_link","motorway_junction","pedestrian","primary","primary_link","raceway","residential","road","secondary","secondary_link","services","tertiary","tertiary_link","track","trunk","trunk_link","turning_circle","turning_loop","unclassified",],
@@ -139,11 +91,13 @@ def fetch_roads(territory):
         roads = ox.features_from_polygon(territory, tags)
     except:
         print('too many roads...')
-        roads = fetch_roads_long(territory,tags)
+        roads = fetch_long_query(territory,tags)
         
     roads = roads.loc[roads.geom_type.isin(['LineString','MultiLineString'])]
     roads = roads.reset_index(drop=True)["geometry"]
 
+    roads = gpd.GeoDataFrame(roads)
+    
     return roads
 
 
@@ -159,6 +113,7 @@ def fetch_water(territory):
             ['Polygon','MultiPolygon','LineString','MultiLineString'])]
         
         water = water.reset_index(drop=True)["geometry"]
+        water = gpd.GeoDataFrame(water)
         
         return water
     except:
@@ -180,6 +135,8 @@ def fetch_railways(territory):
             pass
         
         railway = railway["geometry"]
+        railway  = gpd.GeoDataFrame(railway)
+        
         return railway
     except:
         return
