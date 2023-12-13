d032be157ac77b805e4c6932ff875b8aeb1d2192
Леонид Харлов <harlov.leonid@ya.ru>
2023-12-13 16:17:46 +0300

    Add autofill for city and tag tables

--- urban_scaling_toolkit/db_manager.py
+++ urban_scaling_toolkit/db_manager.py
@@ -1,8 +1,11 @@
 import psycopg2
 from dotenv import load_dotenv
 import os 
-from geopandas import read_postgis
-from pandas import read_sql
+import geopandas as gpd
+import pandas as pd
+from sqlalchemy import create_engine
+
+import utils
 
 load_dotenv()
 
@@ -21,7 +24,20 @@ def init_db():
     create_tag_table()
     create_servicetag_table()
     
-
+    populate_cities_table()
+    populate_tags_table()
+    
+    
+def drop_db(
+    host=HOST,
+    port=PORT,
+    user=USER,
+    dbname=DBNAME,
+    password=PASSWORD):
+    
+    post_query(f'DROP DATABASE {dbname}',dbname=None)
+    
+    
 def post_query(
     query,
     host=HOST,
@@ -67,17 +83,18 @@ def get_query(
         user=user,
         dbname=dbname,
         password=password)
-    
-    cursor = connection.cursor()
-    
+        
     if geom is None and 'geom' in query:
         geom=True
     
     if geom:
-        res = read_postgis(query,con=connection,geom_col='geometry',crs=4326)
+        res = gpd.read_postgis(query,con=connection,geom_col='geometry',crs=4326)
         return res
     
-    res = read_sql(query,con=connection)
+    res = pd.read_sql(query,con=connection)
+    
+    connection.close()
+    
     return res
 
 
@@ -133,10 +150,14 @@ def create_service_table():
         CREATE TABLE IF NOT EXISTS service (
             service_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
             city_id INT,
+            block_id INT,
             name VARCHAR(255),
             geometry geometry(Point, 4326),
             
             CONSTRAINT fk_city FOREIGN KEY(city_id) REFERENCES city(city_id)
+            ON DELETE CASCADE,
+            
+            CONSTRAINT fk_block FOREIGN KEY(block_id) REFERENCES block(block_id)
             ON DELETE CASCADE
         );
         """)
@@ -146,7 +167,6 @@ def create_tag_table():
     post_query("""
         CREATE TABLE IF NOT EXISTS tag (
             tag_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-            servicetag_id INT,
             name VARCHAR(255),
             category VARCHAR(255)               
         );
@@ -154,8 +174,7 @@ def create_tag_table():
     
     
 def create_servicetag_table():
-    post_query([
-        """
+    post_query("""
         CREATE TABLE IF NOT EXISTS servicetag (
             servicetag_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
             service_id INT,
@@ -165,14 +184,9 @@ def create_servicetag_table():
             ON DELETE CASCADE,
             
             CONSTRAINT fk_tag FOREIGN KEY(tag_id) REFERENCES tag(tag_id)
+            ON DELETE CASCADE
         );
-        """,
-        
-        """
-        ALTER TABLE tag 
-        ADD CONSTRAINT fk_servicetag FOREIGN KEY(servicetag_id) REFERENCES servicetag(servicetag_id)
-        """
-    ])
+        """)
     
     
 def create_cluster_view():
@@ -183,3 +197,42 @@ def create_cluster_view():
         JOIN
         GROUP BY
     """)
+    
+    
+def populate_tags_table(
+    service_tags_path='service_tags.json',
+    host=HOST,
+    port=PORT,
+    user=USER,
+    dbname=DBNAME,
+    password=PASSWORD):
+    
+    service_tags_dict = utils.load_json(service_tags_path)
+    service_tags = pd.DataFrame(service_tags_dict)
+    
+    tags = pd.Series({col:service_tags[col].dropna().sum() for col in service_tags.columns}).explode().reset_index()
+    tags.columns = ['category','name']
+    
+    engine = create_engine(
+        f'postgresql://{user}:{password}@{host}:{port}/{dbname}')
+    
+    tags.to_sql('tag',con=engine,if_exists='append',index=False)
+    
+    engine.dispose()
+    
+    
+def populate_cities_table(
+    cities_df_path='agglomerations_data.geojson',
+    host=HOST,
+    port=PORT,
+    user=USER,
+    dbname=DBNAME,
+    password=PASSWORD):
+    
+    engine = create_engine(
+        f'postgresql://{user}:{password}@{host}:{port}/{dbname}')
+    
+    agglomerations = gpd.read_file(cities_df_path)
+    agglomerations.to_postgis('city',con=engine,if_exists='append')
+    
+    engine.dispose()
