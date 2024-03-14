89366e14481f3714d5881c56f5c5e3ef5ef5940e
Леонид Харлов <harlov.leonid@ya.ru>
2024-03-14 21:31:53 +0500

    add function to assess osm data quality

--- urban_scaling_toolkit/metrics.py
+++ urban_scaling_toolkit/metrics.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import pandas as pd
 import momepy as mm
 import numpy as np
-
+import requests
     
 def evaluate_centrality(blocks,services):
     """
@@ -56,3 +56,24 @@ def evaluate_centrality(blocks,services):
     blocks["centrality_bin"] = blocks["centrality_bin"].fillna(0).astype(int)
     
     return blocks
+
+
+# indicators = ['mapping-saturation','currentness']
+# topics = ['building-count','amenities','poi']
+def assess_osm_data_quality(geom, indicator='mapping-saturation',topic='poi'):
+
+    base_url = "https://oqt.ohsome.org/api/indicators/"
+    url = base_url + indicator
+
+    headers = {"accept": "application/json"}
+
+    parameters = {
+        "topic": topic,
+        "bpolys": geom,
+    }
+
+    response = requests.post(url, headers=headers, json=parameters)
+    response.raise_for_status()  # raise an exception if request code is not 200
+    results = response.json()["result"]
+
+    return results[0]['result']['value']
