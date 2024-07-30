af566d333d78caf26066f3a696b568fd0eed3544
Леонид Харлов <harlov.leonid@ya.ru>
2024-07-30 17:02:20 +0500

    update db_manager

--- urban_scaling_toolkit/db_manager.py
+++ urban_scaling_toolkit/db_manager.py
@@ -1,9 +1,15 @@
+import warnings
 import psycopg2
 from dotenv import load_dotenv
 import os 
 import geopandas as gpd
+warnings.filterwarnings('ignore',category=UserWarning)
+
 import pandas as pd
+warnings.filterwarnings('ignore',category=UserWarning)
+
 from sqlalchemy import create_engine
+warnings.filterwarnings('ignore',category=UserWarning)
 
 import utils
 
@@ -11,15 +17,18 @@ load_dotenv()
 
 HOST='localhost'
 PORT=5432
-DBNAME='urban_scaling_db'
+DBNAME='urban_scaling_db_02'
 
 USER=os.environ.get('user')
 PASSWORD=os.environ.get('password')
 
-def init_db():
-    create_urban_scaling_db()
+def init_db(dbname=DBNAME):
+    create_urban_scaling_db(dbname=dbname)
     create_city_table()
     create_block_table()
+    create_road_table()
+    create_railway_table()
+    create_water_table()
     create_service_table()
     create_tag_table()
     create_servicetag_table()
@@ -98,8 +107,8 @@ def get_query(
     return res
 
 
-def create_urban_scaling_db():
-    post_query(f"CREATE DATABASE {DBNAME};",dbname=None)
+def create_urban_scaling_db(dbname=DBNAME):
+    post_query(f"CREATE DATABASE {dbname};",dbname=None)
     post_query('CREATE EXTENSION postgis')
     
 
@@ -127,7 +136,33 @@ def create_road_table():
             ON DELETE CASCADE
         );
         """)
+
+
+def create_railway_table():
+    post_query("""
+        CREATE TABLE IF NOT EXISTS railway (
+            railway_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            city_id INT,
+            geometry geometry(LineString, 4326),
+            
+            CONSTRAINT fk_city FOREIGN KEY(city_id) REFERENCES city(city_id)
+            ON DELETE CASCADE
+        );
+        """)
     
+
+def create_water_table():
+    post_query("""
+        CREATE TABLE IF NOT EXISTS water (
+            water_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            city_id INT,
+            geometry geometry(Geometry, 4326),
+            
+            CONSTRAINT fk_city FOREIGN KEY(city_id) REFERENCES city(city_id)
+            ON DELETE CASCADE
+        );
+        """)
+
     
 def create_block_table():
     post_query("""
@@ -189,16 +224,6 @@ def create_servicetag_table():
         """)
     
     
-def create_cluster_view():
-    post_query("""
-        CREATE VIEW cluster AS
-        SELECT 
-        FROM
-        JOIN
-        GROUP BY
-    """)
-    
-    
 def populate_tags_table(
     service_tags_path='service_tags.json',
     host=HOST,
@@ -222,7 +247,7 @@ def populate_tags_table(
     
     
 def populate_cities_table(
-    cities_df_path='agglomerations_data.geojson',
+    cities_df_path='agglomerations.geojson',
     host=HOST,
     port=PORT,
     user=USER,
@@ -236,3 +261,171 @@ def populate_cities_table(
     agglomerations.to_postgis('city',con=engine,if_exists='append')
     
     engine.dispose()
+  
+    
+def create_shannon_diversity_view():
+    post_query("""
+               CREATE VIEW shannon_diversity AS
+                --  THIS QUERY ADDS SHANNON DIVERSITY MAX-SCALED BY MOST DIVERSE CLUSTER IN A CITY
+                    SELECT
+                        cluster_id,
+                        city_id,
+                        diversity,
+                        diversity/(MAX(diversity) OVER (PARTITION BY city_id)) as diversity_scaled
+                    FROM
+                    (
+                        SELECT
+                    -- 		THIS QUERY RETURNS SHANNON DIVERSITY FOR EVERY CLUSTER
+                            cluster_id,
+                            city_id,
+                            -sum(p*ln(p)) as diversity
+                        FROM 
+                            (
+                    -- 			THIS QUERY RETURNS PROPORTIONS OF EACH TAG IN A CLUSTER
+                                SELECT
+                                block.cluster_id,
+                                block.city_id,
+                                COUNT(servicetag.tag_id)/(SUM(COUNT(servicetag.tag_id)) OVER (PARTITION BY block.cluster_id)) as p
+                                FROM
+                                service
+                                JOIN servicetag ON service.service_id = servicetag.service_id
+                                JOIN block ON block.block_id = service.block_id
+                                GROUP BY
+                                block.city_id, block.cluster_id, servicetag.tag_id
+                            )
+                        GROUP BY city_id, cluster_id
+                    )
+               """)
+    
+
+def create_simpson_diversity_view():
+    post_query("""
+               CREATE VIEW simpson_diversity AS
+                -- THIS QUERY ADDS SIMPSON DIVERSITY MAX-SCALED BY MOST DIVERSE CLUSTER IN A CITY
+                SELECT
+                    cluster_id,
+                    city_id,
+                    diversity,
+                    diversity/(MAX(diversity) OVER (PARTITION BY city_id)) as diversity_scaled
+                FROM
+                (
+                    -- 		THIS QUERY RETURNS SIMPSON DIVERSITY FOR EVERY CLUSTER
+                    SELECT
+                        cluster_id,
+                        city_id,
+                        1-(SUM(tags_of_type*(tags_of_type-1)))/(tags_in_cluster*(tags_in_cluster-1)) as diversity
+                    FROM
+                    (
+                        -- 			
+                        SELECT
+                        block.cluster_id,
+                        block.city_id,
+                        servicetag.tag_id,
+                        COUNT(servicetag.tag_id) as tags_of_type,
+                        SUM(COUNT(servicetag.tag_id)) OVER (PARTITION BY block.cluster_id) as tags_in_cluster
+                        FROM
+                        service
+                        JOIN servicetag ON service.service_id = servicetag.service_id
+                        JOIN block ON block.block_id = service.block_id
+                        GROUP BY
+                        block.city_id, block.cluster_id, servicetag.tag_id
+                    )
+                    WHERE 
+                        tags_in_cluster > 1
+                    GROUP BY 
+                        cluster_id,city_id,tags_in_cluster
+                )
+               """)
+    
+    
+def create_service_count_view():
+    post_query("""
+               CREATE VIEW service_count AS
+                SELECT
+                        block.cluster_id,
+                        block.city_id,
+                        COUNT(service.service_id) as services_total,
+                        CAST(COUNT(service.service_id) AS FLOAT)/(MAX(COUNT(service.service_id)) OVER (PARTITION BY block.city_id)) as services_scaled
+                    FROM
+                        service
+                        JOIN block 
+                        ON block.block_id = service.block_id
+                    WHERE 
+                        cluster_id IS NOT NULL
+                    GROUP BY
+                        block.city_id, block.cluster_id
+               """)
+    
+    
+def create_amenity_saturation_view():
+    post_query("""
+                CREATE VIEW amenity_saturation AS
+                SELECT
+                    cluster_id,
+                    city_id,
+                    diversity_shannon,
+                    diversity_shannon_scaled,
+                    diversity_simpson,
+                    diversity_simpson_scaled,
+                    services_total,
+                    services_scaled,
+                    amenity_saturation_shannon,
+                    amenity_saturation_shannon/(MAX(amenity_saturation_shannon) OVER (PARTITION BY city_id)) as amenity_saturation_shannon_scaled,
+                    amenity_saturation_simpson,
+                    amenity_saturation_simpson/(MAX(amenity_saturation_simpson) OVER (PARTITION BY city_id)) as amenity_saturation_simpson_scaled
+                FROM
+                (
+                    SELECT
+                        dshan.cluster_id,
+                        dshan.city_id,
+                        s.services_total,
+                        s.services_scaled,
+                        dshan.diversity as diversity_shannon,
+                        dshan.diversity_scaled as diversity_shannon_scaled,
+                        dsimp.diversity as diversity_simpson,
+                        dsimp.diversity_scaled as diversity_simpson_scaled,
+                        services_scaled+dshan.diversity_scaled as amenity_saturation_shannon,
+                        services_scaled+dsimp.diversity_scaled as amenity_saturation_simpson
+                    FROM 
+                        shannon_diversity dshan
+                        JOIN simpson_diversity dsimp ON dsimp.cluster_id = dshan.cluster_id
+                        JOIN service_count s ON dshan.cluster_id = s.cluster_id
+                )
+                """)
+    
+    
+def create_cluster_view():
+    post_query("""
+               CREATE VIEW cluster AS
+                SELECT
+                    a.cluster_id,
+                    a.city_id,
+                    c.name,
+                    c.country,
+                    b.population,
+                    b.area,
+                    a.diversity_shannon,
+                    a.diversity_shannon_scaled,
+                    a.diversity_simpson,
+                    a.diversity_simpson_scaled,
+                    a.services_total,
+                    a.services_scaled,
+                    a.amenity_saturation_shannon,
+                    a.amenity_saturation_shannon_scaled,
+                    a.amenity_saturation_simpson,
+                    a.amenity_saturation_simpson_scaled
+                FROM 
+                    amenity_saturation a
+                    JOIN (
+                        SELECT city_id,
+                        name,
+                        country 
+                        FROM city) c ON a.city_id = c.city_id
+                    JOIN (
+                        SELECT 
+                        cluster_id,
+                        SUM(population) as population,
+                        SUM(area) as area 
+                        FROM block 
+                        GROUP BY cluster_id) b ON a.cluster_id = b.cluster_id
+               """)
